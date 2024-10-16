8db81b84221e163efaa91484b4dd39a8339a5f66
TiaraNafisaa <HP@LAPTOP-H9AQT0PT>
2024-10-16 10:49:05 +0700

    laravel initial commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Login</title>
+        <link rel="stylesheet" href="stylelogin.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="login-text">Login</div> <!-- Teks Login dengan font Pattaya -->
+        <form id="loginForm">
+            <div>
+                <label for="username">Username :</label>
+                <input type="text" name="username" id="username">
+            </div>
+            <div>
+                <label for="password">Password :</label>
+                <input type="password" name="password" id="password">  
+            </div>
+            <button type="submit">LOGIN</button>
+        </form>
+        <script src="ASSETS/js/script.js"></script>
+    </body>
+</html>
